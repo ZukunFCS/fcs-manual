3a1daac4902a284fdf45831501005e84704c2c19
miyoomoto <miy_oomoto@toei.co.jp>
2026-02-17 13:26:48 +0900

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -21,7 +21,7 @@ suppress_warnings = ["myst.header"]
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser', 'sphinx_rtd_theme','sphinx_design', ]
+extensions = ['myst_parser', 'sphinx_rtd_theme',]
 if build_pdf:
     extensions.append('rst2pdf.pdfbuilder')
     extensions.append('sphinx.ext.autodoc')
